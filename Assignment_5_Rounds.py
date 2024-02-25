ae586a3f252de98a8c9206ccfa542162137f73be
Isaac Rounds <rounds.isaac@gmail.com>
2024-02-25 17:53:22 -0500

    Added list of tasks that can be accomplished by program

--- Assignment_5_Rounds.py
+++ Assignment_5_Rounds.py
@@ -59,6 +59,54 @@ class BasicMathOperations:
     
     def arg_type(self,arg):
         return type(arg)
+    
+
+    """
+
+Question 3: Write a function that takes three arguments num1, num2 & operator & compute
+the desired operation. Return and show the desired result in your screen.
+Question 4. Write a function that squares its argument.
+Question 5. Write a function that computes factorial of a number.
+Question 6. Write a function that take start and end number as inputs & display counting in
+your screen.
+Question 7. Write a function that computes hypotenuse of a right angle triangle by following
+the steps given below.
+Hypotenuse2 = Base2 + Perpendicular2
+a) Take base and perpendicular as inputs.
+b) Create a function calculateSquare() for calculating and returning square of a
+number.
+c) Create a function calculateHypotenuse() for calculating hypotenuse of a right
+angle triangle. Make use of the calculateSquare() function.
+Question 8. Write a function that calculates the area of a rectangle. A = width * height
+Pass width and height in following manner:
+a) Arguments as values
+b) Arguments as variables
+Question 9. Write a function that computes power of a number. E.g. 23 is 8.
+Question 10. Write a function which accepts an argument and returns the typ
+"""
+    
+vary = BasicMathOperations()
+print("""Tasks accomplished by this program, please enter a number corresponding to the task you want accomplished
+    1)Greet User
+    2)Sum Numbers
+    3)Perform a specified operation on unser-inputted numbers
+    4)Square user inputted number
+    5)Find the facotrial of a user inputted number
+    6)Count from a user provided start to a user prvided end
+    7)Find the hypotonuse of a right triangle, the user inputs its side lengths
+    8)Squares user inputted number
+    9)Find the hypotonuse of a right triangle, the user inputs its side lengths
+    10)raises a user inputed number to a user inputted power
+    11)returns the type of variable the user inputs""")
+    
+
+"""Display a list of all the questions/tasks to the user.
+• Prompt the user to pick a number corresponding to the task they want to execute.
+• Based on the user's choice, call the respective method from the BasicMathOperations class to
+perform the operation.
+• Ensure the program can handle invalid selections and prompt the user again."""
+
+
 
 
 
